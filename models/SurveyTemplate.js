d3fe60eea8d99b1dc857febf203d6509e540fbaa
nejdet123 <nejdettilimsani@gmail.com>
2025-04-15 15:04:30 +0300

    Update SurveyTemplate.js

--- models/SurveyTemplate.js
+++ models/SurveyTemplate.js
@@ -1,23 +1,15 @@
 const mongoose = require('mongoose');
 
-const SurveyTemplateSchema = new mongoose.Schema({
-  key: {
-    type: String,
-    required: true,
-    unique: true // مثال: "orphans-training-survey"
-  },
-  title: {
-    type: String,
-    required: true
-  },
+const surveyTemplateSchema = new mongoose.Schema({
+  title: String,
   structure: {
-    type: Object,
-    required: true // يحتوي على surveyIntro, sections, surveyFooter...
-  },
-  updatedAt: {
-    type: Date,
-    default: Date.now
+    survey: {
+      title: String,
+      description: String,
+      instructions: [String]
+    },
+    sections: Array
   }
 });
 
-module.exports = mongoose.model('SurveyTemplate', SurveyTemplateSchema);
+module.exports = mongoose.model('SurveyTemplate', surveyTemplateSchema);
