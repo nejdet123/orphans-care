f89bfd25a9d4a4ac573a6c772dd25ffd4da97029
nejdet123 <nejdettilimsani@gmail.com>
2025-04-15 16:32:03 +0300

    Update SurveyResponse.js

--- models/SurveyResponse.js
+++ models/SurveyResponse.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const SurveyResponseSchema = new mongoose.Schema({
-  surveyKey: String,         // مفتاح الاستبيان (مثلاً: orphans-training-survey)
-  answers: mongoose.Schema.Types.Mixed, // تخزين الأجوبة بأي شكل
+  surveyKey: String,
+  answers: mongoose.Schema.Types.Mixed,
   submittedAt: { type: Date, default: Date.now }
 });
 
