188310a5b45d42c597d46f43d5837100d386ddca
nejdet123 <nejdettilimsani@gmail.com>
2025-04-17 15:47:02 +0300

    Update SurveyTemplate.js

--- models/SurveyTemplate.js
+++ models/SurveyTemplate.js
@@ -1,15 +1,26 @@
+// File: models/SurveyTemplate.js
 const mongoose = require('mongoose');
 
 const SurveyTemplateSchema = new mongoose.Schema({
-  key: String,
+  key: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  title: {
+    type: String
+  },
   structure: {
     survey: {
       title: String,
       description: String,
       instructions: [String],
-      sections: Array
+      sections: [mongoose.Schema.Types.Mixed],
+      footer: mongoose.Schema.Types.Mixed
     }
   }
+}, {
+  timestamps: true
 });
 
 module.exports = mongoose.model('SurveyTemplate', SurveyTemplateSchema);
